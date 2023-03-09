ab0ea6dc92abd205ead6d0972ed6342f78398eb2
KrushnaBodake <bodakekrushna9@gmail.com>
2023-03-09 17:51:31 +0530

    Add project files.

---
+++ AuthenticationDemo/Data/Migrations/20230303051027_first-migration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
+
+#nullable disable
+
+namespace AuthenticationDemo.Data.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
